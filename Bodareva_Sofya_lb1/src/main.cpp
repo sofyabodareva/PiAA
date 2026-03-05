7c69b006f1a9211b137f6f35303443e2acdff834
sofyabodareva <bodareva03@gmail.com>
2026-03-06 00:04:43 +0300

    добавление промежуточных выводов

--- Bodareva_Sofya_lb1/src/main.cpp
+++ Bodareva_Sofya_lb1/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <windows.h>
 
 class Piece{
 public:
@@ -9,7 +10,7 @@ public:
     Piece(int column, int row, int pieceSize):column(column), row(row), pieceSize(pieceSize){}
 
     friend std::ostream& operator << (std::ostream& out, const Piece& piece){
-        return out << piece.column << " " << piece.row << " " << piece.pieceSize;
+        return out << "(" << piece.column << ", " << piece.row << ") " << piece.pieceSize;
     }
 };
 
@@ -24,7 +25,7 @@ class Table{
 
 
     int findMinDivisor(int n){
-        for(int i{2}; i< n; ++i){
+        for(int i{3}; i< n; i+=2){
             if(n % i == 0){
                 return i;
             }
@@ -42,19 +43,29 @@ class Table{
 
     void backtracking(int column, int row, int filledPart) {
         ++iterations;
+        std::cout << "Вход в функцию backtracking №" << iterations <<
+        "\nКоординаты начала поиска: (" << column << ", " << row
+        << ")\nПлощадь заполненной части: " << filledPart
+        << "\nКоличество квадратов в текущем разбиении: " << currentSplitting.size() << "\n";
         for (int y{row}; y <= size; ++y) {
             for (int x{column}; x <= size; ++x) {
                 if (!isUsed(x, y)) {
+                    std::cout << "Координаты вставки нового квадрата: (" << column
+                    << ", " << row << ")\n";
                     int squareSize = std::min(size - x,  size - y) + 1;
                     for (const Piece& piece : currentSplitting) {
                         if (piece.row + piece.pieceSize > y && piece.column > x)
                             squareSize = std::min(squareSize, piece.column - x);
                     }
+                    std::cout << "Максимальный размер квадрата для вставки: " << squareSize << "\n";
                     for (int s{squareSize}; s > 0; --s) {
                         currentSplitting.push_back(Piece(x, y, s));
+                        std::cout << "Добавляем в разбиение квадрат: (" << x << ", "
+                        << y << ") " << s << "\n";
                         if (filledPart + s * s == size * size) {
                             if (currentSplitting.size() < bestSplitting.size() || bestSplitting.size() == 0) {
                                 bestSplitting = currentSplitting;
+                                std::cout << "Текущий размер лучшего разбиения: " << currentSplitting.size() << "\n";
                             }
                         }
                         else {
@@ -62,6 +73,8 @@ class Table{
                                 backtracking(x + s, y, filledPart + s * s);
                             }
                         }
+                        std::cout << "Возвращаемся назад - удаляем последний квадрат: (" 
+                        << x << ", " << y << ") " << s << "\n";
                         currentSplitting.pop_back();
                     }
                     return;
@@ -85,7 +98,8 @@ public:
     Table(int tableSize):tableSize(tableSize), size(findMinDivisor(tableSize)), scale(tableSize/size), iterations(0){}
 
     friend std::ostream& operator << (std::ostream& out, const Table& table){
-        out << table.bestSplitting.size() << "\n";
+        out << "Минимальное количество квадратов: " << table.bestSplitting.size() << "\n";
+        out << "Координаты левых верхних углов использованных квадратов и длины их сторон:\n";
         for(int i{0}; i < table.bestSplitting.size(); ++i){
             out << table.bestSplitting[i] << "\n";
         }
@@ -94,6 +108,7 @@ public:
 
     void findBestSplitting(){
         if(tableSize % 2 == 0){
+            std::cout << "Сторона стола - чётное число, лучшее разбиение - 4 равных квадрата\n";
             bestSplitting.push_back(Piece(1, 1, tableSize/2));
             bestSplitting.push_back(Piece(tableSize/2+1, 1, tableSize/2));
             bestSplitting.push_back(Piece(1, tableSize/2+1, tableSize/2));
@@ -112,11 +127,12 @@ public:
 
 
 int main(){
+    SetConsoleOutputCP(CP_UTF8);
     int n;
     std::cin >> n;
     Table t(n);
     t.findBestSplitting();
+    std::cout<< "Количество итераций: " << t.iterations << "\n";
     std::cout << t;
-    std::cout<< t.iterations;
     return 0;
 }
