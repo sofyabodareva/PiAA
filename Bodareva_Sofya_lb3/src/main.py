2a6a56b1e97d73587a540925b1c4674fe943b420
sofyabodareva <bodareva03@gmail.com>
2026-03-10 13:13:46 +0300

    Промежуточные выводы

--- Bodareva_Sofya_lb3/src/main.py
+++ Bodareva_Sofya_lb3/src/main.py
@@ -20,14 +20,20 @@ def LevenshteinDistance(str1: str, str2: str, ch_replace: str, replace: int, ch_
     m = len(str1)
     n = len(str2)
     previous = [x for x in range(n+1)]
+    print(f"Начальная строка: {previous}")
     current = [0] * (n + 1)
     for i in range(1, m+1):
         current[0] = i
+        print(f"Обрабатываемый символ: '{str1[i-1]}', (i = {i})")
+        print(f"Текущая строка: {current}")
         for j in range(1, n+1):
             current[j] = min(current[j-1]+insert_cost(str2[j-1], ch_insert, insert),
                             previous[j]+1,
                             previous[j-1] + replace_cost(str1[i-1], str2[j-1], ch_replace, replace))
+            print(f"Сравнение '{str1[i-1]}' и '{str2[j-1]}' -> current[{j}] = {current[j]}")
+        print(f"Готовая строка матрицы: {current}")
         previous, current = current, previous
+        print(f"Новая начальная строка: {previous}")
     return previous[n]
 
 
